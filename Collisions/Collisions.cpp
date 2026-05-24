7573f1461fe4f249ddd1faf681c61fc0cd3f58d2
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-05-24 05:50:31 +0100

    Added pan/zoom, clear world and draw world code

--- Collisions/Collisions.cpp
+++ Collisions/Collisions.cpp
@@ -15,20 +15,85 @@ public:
 private:
 	olc::TileTransformedView tv;
 
+	struct sWorldObject
+	{
+		olc::vf2d vPos;
+		olc::vf2d vVel;
+		float fRadius = 0.5f;
+	};
+
+	sWorldObject object;
+
+	std::string sWorldMap = 
+		"################################"
+		"#..............................#"
+		"#.......#####.#.....#####......#"
+		"#.......#...#.#.....#..........#"
+		"#.......#...#.#.....#..........#"
+		"#.......#####.#####.#####......#"
+		"#..............................#"
+		"#.....#####.#####.#####..##....#"
+		"#.........#.#...#.....#.#.#....#"
+		"#.....#####.#...#.#####...#....#"
+		"#.....#.....#...#.#.......#....#"
+		"#.....#####.#####.#####.#####..#"
+		"#..............................#"
+		"#..............................#"
+		"#..#.#..........#....#.........#"
+		"#..#.#..........#....#.........#"
+		"#..#.#.......#####.#######.....#"
+		"#..#.#..........#....#.........#"
+		"#..#.#.............###.#.#.....#"
+		"#..#.##########................#"
+		"#..#..........#....#.#.#.#.....#"
+		"#..#.####.###.#................#"
+		"#..#.#......#.#................#"
+		"#..#.#.####.#.#....###..###....#"
+		"#..#.#......#.#....#......#....#"
+		"#..#.########.#....#......#....#"
+		"#..#..........#....#......#....#"
+		"#..############....#......#....#"
+		"#..................########....#"
+		"#..............................#"
+		"#..............................#"
+		"################################";
+
+	olc::vi2d vWorldSize = { 32, 32 };
+
 
 public:
 	bool OnUserCreate() override
 	{
 		tv = olc::TileTransformedView({ ScreenWidth(), ScreenHeight() }, { 32, 32 });
+		object.vPos = { 3.0f, 3.0f };
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+		// Pan & Zoom:
+		if (GetMouse(2).bPressed) tv.StartPan(GetMousePos());
+		if (GetMouse(2).bHeld) tv.UpdatePan(GetMousePos());
+		if (GetMouse(2).bReleased) tv.EndPan(GetMousePos());
+		if (GetMouseWheel() > 0) tv.ZoomAtScreenPos(2.0f, GetMousePos());
+		if (GetMouseWheel() < 0) tv.ZoomAtScreenPos(0.5f, GetMousePos());
+
 		// Clear world
 		Clear(olc::VERY_DARK_BLUE);
 		
-		tv.DrawCircle({ 0, 0 }, 20.0f);
+		// Draw world:
+		olc::vi2d vTL = tv.GetTopLeftTile().max({ 0, 0 });
+		olc::vi2d vBR = tv.GetBottomRightTile().min(vWorldSize);
+		olc::vi2d vTile;
+		for (vTile.y = vTL.y; vTile.y < vBR.y; vTile.y++)
+			for (vTile.x = vTL.x; vTile.x < vBR.x; vTile.x++)
+			{
+				if (sWorldMap[vTile.y * vWorldSize.x + vTile.x] == '#')
+				{
+
+				}
+			}
+
 
 		return true;
 	}
